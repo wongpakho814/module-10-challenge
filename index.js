e019943caf2e1736c92a35d8fc0a8ee0000f9cac
wongpakho814 <wongpakho814@gmail.com>
2022-10-17 16:00:25 +1100

    Updated file structure

--- index.js
+++ index.js
@@ -1,19 +1,11 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generateContent = require('./utils/generateMTML.js');
+const generateContent = require('./lib/generateMTML.js');
 const fileName = "index.html";
 
-// TODO: Create an array of questions for user input
+// An array of questions for user input
 const questions = [
     "What is your project's title?",
-    "Description of the project (e.g. What was your motivation? What problem does it solve?): ",
-    "What are the steps required to install your project?",
-    "The instructions and examples for use: ",
-    "Please list the collaborators here: ",
-    "Please provide examples on how to run the tests, if any: ",
-    "What is your GitHub username?",
-    "What is your email address for contact use?",
-    "Please choose the license used in this project, if any: ",
 ];
 
 // TODO: Create a function to write README file    
