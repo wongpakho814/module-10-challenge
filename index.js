f2fd2b553d48712a8a0dddd2705e8d789c98d854
wongpakho814 <wongpakho814@gmail.com>
2022-10-17 17:39:26 +1100

    Implemented code to create index.html, and render the manager to it

--- index.js
+++ index.js
@@ -1,93 +1,107 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
-const generateContent = require('./lib/generateMTML.js');
-const fileName = "index.html";
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateContent = require("./lib/generateHTML.js");
+const Manager = require("./lib/manager.js");
+const fileName = "./dist/index.html";
 
-// An array of questions for user input
-const questions = [
-    "What is your project's title?",
+// An array of questions for the team manager's information for user input
+const managerQuestions = [
+  "What is the team manager's name?",
+  "What is the team manager's employee ID?",
+  "What is the team manager's email address?",
+  "What is the team manager's office number?",
+  "Please pick an option to add an engineer, or an intern, or to finish building your team: ",
 ];
 
-// TODO: Create a function to write README file    
-function writeToFile(fileName, data) {
-    fs.writeFile(fileName, generateContent.generateHTML(data), (err) =>
-        err ? console.log(err) : console.log('Successfully created index.html!')
-    );
+// An array of questions for the engineer's information for user input
+const engineerQuestions = [
+  "What is the engineer's name?",
+  "What is the engineer's employee ID?",
+  "What is the engineer's email address?",
+  "What is the engineer's GitHub username?",
+];
+
+// Function to initiate the HTML file, and write the manager data to it
+function writeManagerToFile(fileName, manager) {
+  fs.writeFile(fileName, generateContent.generateHTML(manager), (err) =>
+    err ? console.log(err) : console.log("Successfully created index.html!")
+  );
 }
 
-// TODO: Create a function to initialize app
-function init() {
-    inquirer
-        .prompt([
-            {
-                type: "input",
-                name: "title",
-                message: questions[0],
-            },
-            {
-                type: "input",
-                name: "description",
-                message: questions[1],
-            },
-            {
-                type: "input",
-                name: "installation",
-                message: questions[2],
-            },
-            {
-                type: "input",
-                name: "usage",
-                message: questions[3],
-            },
-            {
-                type: "input",
-                name: "credits",
-                message: questions[4],
-            },
-            {
-                type: "input",
-                name: "tests",
-                message: questions[5],
-            },
-            {
-                type: "input",
-                name: "username",
-                message: questions[6],
-            },
-            {
-                type: "input",
-                name: "email",
-                message: questions[7],
-            },
-            {
-                type: "list",
-                name: "license",
-                message: questions[8],
-                choices: [
-                    "none",
-                    "apache-2.0", 
-                    "bsl-1.0",
-                    "bsd-3-clause", 
-                    "bsd-2-clause", 
-                    "cc0-1.0", 
-                    "cc-by-4.0", 
-                    "cc-by-sa-4.0",
-                    "epl-1.0",
-                    "gpl-3.0",
-                    "gpl-2.0",
-                    "agpl-3.0",
-                    "isc",
-                    "mit",
-                    "mpl-2.0",
-                    "ofl-1.1",
-                    "unlicense",
-                    "zlib"
-                ],
-            },
-        ])
-        .then((data) => {
-            writeToFile(fileName, data);
-        });
+// Function that acts as the menu for user to add more engineer/intern until the finish option is chosen
+const showMenu = () => {
+  return inquirer.prompt({
+    type: "list",
+    name: "members",
+    message: managerQuestions[4],
+    choices: ["engineer", "intern", "finish"],
+  });
+};
+
+// Function to initialize app
+async function init() {
+  let isFinished = false;
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "managerName",
+        message: managerQuestions[0],
+      },
+      {
+        type: "input",
+        name: "managerID",
+        message: managerQuestions[1],
+      },
+      {
+        type: "input",
+        name: "managerEmail",
+        message: managerQuestions[2],
+      },
+      {
+        type: "input",
+        name: "managerOfficeNum",
+        message: managerQuestions[3],
+      },
+    ])
+    .then((data) => {
+      const manager = new Manager(data.managerName, data.managerID, data.managerEmail, data.managerOfficeNum);
+      writeManagerToFile(fileName, manager);
+    });
+    // .then(({ members }) => {
+    //   if (members === "engineer") {
+    //     inquirer
+    //       .prompt([
+    //         {
+    //           type: "input",
+    //           name: "engineerName",
+    //           message: engineerQuestions[0],
+    //         },
+    //         {
+    //           type: "input",
+    //           name: "engineerID",
+    //           message: engineerQuestions[1],
+    //         },
+    //         {
+    //           type: "input",
+    //           name: "engineerEmail",
+    //           message: engineerQuestions[2],
+    //         },
+    //         {
+    //           type: "input",
+    //           name: "engineerGitHub",
+    //           message: engineerQuestions[3],
+    //         },
+    //         {
+    //           type: "list",
+    //           name: "members",
+    //           message: engineerQuestions[4],
+    //           choices: ["engineer", "intern", "finish"],
+    //         },
+    //       ])
+    //       .then({});
+    //   }
+    // });
 }
 
 // Function call to initialize app
